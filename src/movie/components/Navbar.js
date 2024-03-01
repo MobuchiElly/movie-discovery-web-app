07434b6f5f3b480c62d322d5efe527af2a227daa
MobuchiElly <ugwueleazer@gmail.com>
2024-03-01 15:42:50 +0100

    improved responsiveness on smaller screens

--- src/movie/components/Navbar.js
+++ src/movie/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-dark text-light shadow-lg p-4'>
         <div className='container-fluid d-flex flex-column justify-content-center'>
-            <h1 className='text-center w-full fst-italic'>Netsonia</h1>
+            <h1 className='text-center w-full fst-italic' style={{fontSize:'32px'}}>Netsonia</h1>
             <span className='text-center'>Discover, Indulge and Explore the World of Movies</span>
         </div>
     </nav>
